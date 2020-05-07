e07b18435e0a1d325dbe8cf9e627f2264f7e445c
罗晏宸 <yc1023601986@mail.ustc.edu.cn>
2020-05-07 20:10:07 +0800

    增加了用户和节日的模型

--- HolidayQuery/user/models.py
+++ HolidayQuery/user/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    username = models.CharField(max_length=50)
+    password = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.user_name
+
+
+class Holiday(models.Model):
+    holiday_name = models.CharField(max_length=50)
+    holiday_date = models.DateField(max_length=50)
+
+    def __str__(self):
+        return self.holiday_name
